63b5a9527609a438183606257b25979ac5ae66d9
andreiagligor22 <alinmamitati@yahoo.com>
2019-07-27 12:21:35 +0300

    saptamana7

---
+++ exercitii js/exe2.js
@@ -0,0 +1,5 @@
+var z = prompt('What is the input string?');
+var output1 = 'What is the input string?' + z;
+var output2 = z + 'has' + z.length + 'charaters';
+console.log(output1);
+console.log(output2);
