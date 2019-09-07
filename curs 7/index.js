4b1e880be445a7aab084377b1a4c63dafa2bbb15
Mihaela03 <miha_dana09@yahoo.com>
2019-09-07 12:58:26 +0300

    JS

---
+++ curs 7/index.js
@@ -0,0 +1,16 @@
+let string = 'this is a string';
+let number = 2174136461;
+let boolean = true;
+
+let array = ['213', 123, 'something'];
+let object = {
+	value1: 'something',
+	value2: 21312,
+	value3: true
+}
+console.log(string);
+console.log(number);
+console.log(boolean);
+console.log(array[1]);
+console.log(object);
+console.log(object.value1);
