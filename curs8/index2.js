bbefd7a44cffdce0f2a33eaa3d821f1b7a882980
nicoletatamas <tamasnicoleta202@yahoo.com>
2019-09-14 12:00:30 +0300

    js

---
+++ curs8/index2.js
@@ -0,0 +1,25 @@
+console.log(document);
+const getId = document.getElementById('idName');
+console.log(getId);
+const getClass = document.getElementsByClassName('className');
+console.log(getClass);
+const getTag = document.getElementsByTagName('div');
+console.log(getTag);
+
+//element.innerHTML
+console.log(getId.innerHTML);
+getId.innerHTML += '<div>3</div>';
+
+for(let i = 0; i < getClass.length ; i++) {
+ 	getClass[i].innerHTML = i + 1;
+}
+
+//attribute
+const idAttr = getId.getAttribute('id');
+console.log(idAttr);
+
+const idAttr2 = getId.getAttribute('data-id');
+console.log(idAttr2);
+getId.setAttribute('data-attr', 'something');
+getId.style.background = 'red';
+
