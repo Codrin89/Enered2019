590905d09ac4ff2585eae8ed3fcc051bc34f1ae4
nicoletatamas <tamasnicoleta202@yahoo.com>
2019-09-21 12:28:00 +0300

    js

--- curs9/index.js
+++ curs9/index.js
@@ -27,11 +27,12 @@ function initPage() {
 		container.innerHTML += '<div class="row"><div class="column">'+
 		getData[i].name+'</div><div class="column">'+getData[i].
 		hour+'</div><div class="column">'+getData[i].day+
-		'</div><div class="column">'+getData[i].todo+'</div><button class="btn-update">update</button><button class="btn-delete"data-id="'+i+'">delete</button></div>';
+		'</div><div class="column">'+getData[i].todo+'</div><button class="btn-update" data-id="'+i+'">update</button><button class="btn-delete"data-id="'+i+'">delete</button></div>';
 	}
 }
 function addEventListenerRows() {
 	const deleteButtons = document.getElementsByClassName('btn-delete');
+	const updateButtons = document.getElementsByClassName('btn-update');
 	for(let i = 0 ; i < deleteButtons.length ; i++) {
 		deleteButtons[i].addEventListener('click', function(event) {
 			const index = event.target.getAttribute('data-id');
@@ -41,7 +42,76 @@ function addEventListenerRows() {
 			window.location.reload();
 
 		});
+		updateButtons[i].addEventListener('click', function(event){
+			const index = event.target.getAttribute('data-id');
+			window.localStorage.setItem('item-update', index);
+			const form = document.getElementsByClassName('update-form')[0];
+			form.classList.remove('displayNone');
+			// index --> data row
+			const data = JSON.parse(window.localStorage.getItem('todo-data'));
+			document.getElementById('name-update').value = data[index].name;
+			document.getElementById('hour-update').value = data[index].hour;
+			document.getElementById('day-update').value = data[index].day;
+			document.getElementById('todo-update').value = data[index].todo;
+
+		});
 	}
 }
+
+document.getElementById('btn-save-update').addEventListener('click', function(event) {
+	const data = JSON.parse(window.localStorage.getItem('todo-data'));
+	const index = window.localStorage.getItem('item-update');
+	data[index] = {
+		'name': document.getElementById('name-update').value,
+		'hour': document.getElementById('hour-update').value,
+		'day': document.getElementById('day-update').value,
+		'todo': document.getElementById('todo-update').value,
+	};
+	window.localStorage.setItem('todo-data', JSON.stringify(data));
+	window.location.reload();
+
+});
+
 initPage();
 addEventListenerRows();
+
+
+document.getElementById('btn-sort').addEventListener('click', function(event) {
+	const data = JSON.parse(window.localStorage.getItem('todo-data'));
+	data.sort(function(a , b) {
+		if(a.name < b.name) {
+			return 1;
+		} else {
+			return -1;
+		}
+
+	});
+	window.localStorage.setItem('todo-data', JSON.stringify(data));
+	window.location.reload();
+})
+document.getElementById('btn-sort-hour').addEventListener('click', function(event) {
+	const data = JSON.parse(window.localStorage.getItem('todo-data'));
+	data.sort(function(a , b) {
+		if(a.hour < b.hour) {
+			return 1;
+		} else {
+			return -1;
+		}
+
+	});
+	window.localStorage.setItem('todo-data', JSON.stringify(data));
+	window.location.reload();
+})
+document.getElementById('btn-sort-day').addEventListener('click', function(event) {
+	const data = JSON.parse(window.localStorage.getItem('todo-data'));
+	data.sort(function(a , b) {
+		if(a.day < b.day) {
+			return 1;
+		} else {
+			return -1;
+		}
+
+	});
+	window.localStorage.setItem('todo-data', JSON.stringify(data));
+	window.location.reload();
+})
