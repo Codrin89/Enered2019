8b89125755a657d9dd5b2d3b3f628045d21e8b18
nicoletatamas <tamasnicoleta202@yahoo.com>
2019-09-22 12:18:16 +0300

    js

---
+++ curs10/index.js
@@ -0,0 +1,70 @@
+ const getData = [ 'FrontEnd', 'Backend', 'DevOps'];
+ const container = document.getElementById('profession');
+ for(let i = 0; i < getData.length; i++) {
+ 	container.innerHTML += '<option value="'+getData[i]+'">'+getData[i]+'</option>';
+ }
+ document.getElementById('age').addEventListener('keyup', function(event) {
+ 	const value = event.target.value;
+ 	const resultValidate = validateAge(value);
+ 	if(resultValidate !== value) {
+ 		event.target.value = resultValidate;
+ 	}
+
+ 	     
+ });
+ function validateAge(value){
+ 		const regExp = new RegExp('^\\d+$');
+ 		const z = regExp.test(value); 
+ 		if (z) {
+ 			return value;
+ 		} else {
+ 			return value.substr(0, value.length-1);
+ 		}
+ };
+  document.getElementById('name').addEventListener('keyup', function(event) {
+ 	const value = event.target.value;
+ 	const resultValidate = validateName(value);
+ 	if(resultValidate !== value) {
+ 		event.target.value = resultValidate;
+ 	}
+
+ 	     
+ });
+ function validateName(value){
+ 		const regExp = new RegExp('^[a-zA-Z]+$');
+ 		const z = regExp.test(value); 
+ 		if (z) {
+ 			return value;
+ 		} else {
+ 			return value.substr(0, value.length-1);
+ 		}
+ };
+
+document.getElementById('years_exp').addEventListener('keyup', function(event) {
+ 	const value = event.target.value;
+ 	const resultValidate = validateYears_exp(value);
+ 	if(resultValidate !== value) {
+ 		event.target.value = resultValidate;
+ 	}
+
+ 	     
+ });
+ function validateYears_exp(value){
+ 		const regExp = new RegExp('^\\d+$');
+ 		const z = regExp.test(value); 
+ 		if (z && value.length <3) {
+ 			return value;
+ 		} else {
+ 			return value.substr(0, value.length-1);
+ 		}
+ };
+
+document.getElementById('button').addEventListener('click', function(){
+	const saveData = {
+		'name': document.getElementById('name').value,
+		'age': document.getElementById('age').value,
+		'years_exp': document.getElementById('years_exp').value,
+		'birthdate': document.getElementById('birthdate').value,
+	};
+	console.log(saveData);
+});
