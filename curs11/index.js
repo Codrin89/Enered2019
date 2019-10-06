0932157d28e534e4cfa6f7a726c0b006ff9b3489
Mihaela03 <miha_dana09@yahoo.com>
2019-10-06 12:24:25 +0300

    API

---
+++ curs11/index.js
@@ -0,0 +1,71 @@
+var data1 = {
+  "Characters": [
+    {
+      "id": "1",
+      "name": "Captain America"
+    },
+    {
+      "id": "2",
+      "name": "Spider Man"
+    },
+    {
+      "id": "3",
+      "name": "Thor"
+    },
+    {
+      "id": "4",
+      "name": "Storm"
+    },
+    {
+      "id": "5",
+      "name": "Invisible Woman"
+    },
+    {
+      "id": "6",
+      "name": "Ultron"
+    },
+    {
+      "id": "7",
+      "name": "Loki"
+    },
+    {
+      "id": "8",
+      "name": "Magneto"
+    },
+    {
+      "id": "9",
+      "name": "Mystique"
+    },
+    {
+      "id": "10",
+      "name": "Ronan"
+    }
+  ]
+};
+
+console.log(data1);
+
+function renderSuperheroeslist(dataFeed) {
+  
+  for(let i = 0; i < dataFeed.length ; i++) {
+    console.log(dataFeed[i]);
+    var markup = '<div class="row"><div class="column">' +dataFeed[i].id+ '</div><div class="column">' +dataFeed[i].name+ '</div><button class="btn btn-goTo" data-id="'+dataFeed[i].id+'">Go to superhero page</button></div>';
+   document.getElementsByClassName('superheroes-list')[0].innerHTML += markup;
+
+  }
+  addEventlistenerButtons();
+
+}
+
+
+renderSuperheroeslist(data1.Characters);
+
+function addEventlistenerButtons() {
+  const buttons = document.getElementsByClassName('btn-goTo');
+  for (let i = 0; i <buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+      console.log(this.dataset.id);
+      window.location.href = 'singleHero.html';
+    });
+  }
+}
