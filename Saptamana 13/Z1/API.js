08112169e8bb76a048de7141f50ec30e2bf5492f
Claudiu <zapp3r_yo@yahoo.com>
2019-10-19 11:52:49 +0300

    Saptamana13

---
+++ Saptamana 13/Z1/API.js
@@ -0,0 +1,39 @@
+function renderSuperheroesList(dataFeed) {
+
+  for(let i = 0; i < dataFeed.length ; i++) {
+    var markup = '<div class="row"><div class="column">' +dataFeed[i].id+ '</div><div class="column">' +dataFeed[i].name+ '</div><button class="btn btn-goTo" data-id="'+dataFeed[i].id+'">Go to superhero page</button></div>';
+    document.getElementsByClassName('superheroes-list')[0].innerHTML += markup;
+  }
+  addEventListenerButtons();
+}
+
+// renderSuperheroesList(data1.Characters);
+
+function addEventListenerButtons() {
+  const buttons = document.getElementsByClassName('btn-goTo');
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+      console.log(this.dataset.id);
+      window.location.href = 'APISuperheroes2.html>id =' + this.dataset.id;
+    });
+  }
+}
+
+function callServer() {
+  var xhr = new XMLHttpRequest();
+  const id = window.location.href.split('?id=')[1];
+  const url = 'http://private-anon-e98eb24d32-superheroes.apiary-mock.com/characters/';
+  
+  xhr.onreadystatechange = function(response) {
+    console.log(response.currentTarget.readyState);
+    if(response.currentTarget.readyState === 4){
+      var result = JSON.parse(response.currentTarget.responseText);
+      renderSuperheroesList(result.Characters);
+    }
+
+  }
+  xhr.open("GET", url, true);
+  xhr.send();
+}
+
+callServer(); 
---
+++ Saptamana 13/Z1/API2.js
@@ -0,0 +1,36 @@
+function renderHeroData(singleHeroData) {
+	const elementToAppend = document.getElementsByClassName('hero-container')[0];
+	elementToAppend.innerHTML += '<h1>' + singleHeroData.name + '</h1>';
+	elementToAppend.innerHTML += '<img class="hero-picture" src="' + singleHeroData.picture + '"/>';
+	elementToAppend.innerHTML += '<p>' + singleHeroData.description + '</p>';
+	elementToAppend.innerHTML += '<div class="headline">Alliases</div>'
+	const aliases = singleHeroData.aliases;
+	for(let i = 0; i < aliases.length; i++) {
+		elementToAppend.innerHTML += '<p>'+ aliases[i] +'</p>';
+	}
+	elementToAppend.innerHTML += '<div class="headline">Comics</div>'
+	const comics = singleHeroData.comics;
+	console.log(comics);
+	for(let i = 0; i < comics.length; i++) {
+		elementToAppend.innerHTML += '<p>'+ comics[i].name +'</p>';
+	}
+}
+
+function callServer() {
+  var xhr = new XMLHttpRequest();
+  const id = window.location.href.split('?id=')[1];
+  const url = 'http://private-anon-e98eb24d32-superheroes.apiary-mock.com/characters/' + id;
+  
+  xhr.onreadystatechange = function(response) {
+    console.log(response.currentTarget.readyState);
+    if(response.currentTarget.readyState === 4){
+      var result = JSON.parse(response.currentTarget.responseText);
+      renderHeroData(result);
+    }
+
+  }
+  xhr.open("GET", url, true);
+  xhr.send();
+}
+
+callServer(); 
