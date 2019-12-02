5380cd55d4fb88db3e657f1e59dae843b929a094
Gabriela Butnariu <gabriela.butnariu@feelit.iasi.net>
2019-12-02 13:57:30 +0200

    exe cel mai lung sir de numere

---
+++ S18Z2/exe.js
@@ -0,0 +1,21 @@
+let arrayOfNumbers = [1,2,3,5,6,1,3,2,4,5,6,8,12,13,14,42,1,4,3,5,6,7];
+let currentRow = 1;
+let longestRow = 0;
+let firstIndex = 0;
+let winnerIndex = 0;
+
+for (let i=0; i<(arrayOfNumbers.length-1); i++) {
+	if(arrayOfNumbers[i]<arrayOfNumbers[i+1]) {
+		if(currentRow===1) {
+			firstIndex=i;
+		}
+		currentRow++;
+	} else if (currentRow>longestRow) {
+		longestRow = currentRow;
+		winnerIndex = firstIndex;
+		currentRow = 1;
+	} else {
+		currentRow = 1;
+	}
+}
+console.log(arrayOfNumbers.slice(winnerIndex, (winnerIndex+longestRow)));
