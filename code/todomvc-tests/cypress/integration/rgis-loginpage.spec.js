24bd3c578719148bd3bb8739e3fd3ad1450fbc78
Gabriela Butnariu <gabriela.butnariu@feelit.iasi.net>
2019-12-13 16:30:59 +0200

    rgis

---
+++ code/todomvc-tests/cypress/integration/rgis-loginpage.spec.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+
+    it('should ago to main page', () => {    
+        cy.visit('http://ngen-bo-dev.feelit.iasi.net/login') 
+        cy.get('#inputUsername').type('test.test')
+        cy.get('#inputPassword').type('Parola123+{enter}')
+
+        cy.get(':nth-child(5) > .nav-button').click()
+
+        cy.get('.level-title').click()
+
+        cy.get('.level-drop > .ReactTable > .rt-table > .rt-tbody > :nth-child(1) > .rt-tr > [style="flex: 100 0 auto; width: 100px;"]').click()
+
+        cy.get(':nth-child(1) > .action-button > .button-img').click()
+
+        cy.get('.save-button').click()
+
+        cy.get('.ok-button').click()
+
+    })
--- cypress/todomvc-tests/cypress/integration/todomvc.spec.js
+++
@@ -1,6 +0,0 @@
-/// <reference types="cypress" />
-
-it('should navigate to the ToDoMVC App', () => {
-    cy.visit('http://todomvc-app-for-testing.surge.sh/')
-})
-
