3e7fef66e07bcf05e224fcde51e4f7ca3a1d61d1
aldehida <gabrielastoian@rocketmail.com>
2020-01-12 15:06:59 +0200

    bestvibes design

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt me!</h1>
-      <Pet name="Luna" animal="dog" breed="havanese" />
-      <Pet name="Petrisor" animal="cat" breed="european" />
-      <Pet name="Dudu" animal="cat" breed="mixed" />
+      <SearchParams />
     </div>
   );
 };
---
+++ adopt-me/src/SearchParams.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
+          ></input>
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)} //for screen readers; accessibility
+          >
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option key="animal" value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
